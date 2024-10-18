f211073d896b550cb56ea3161b2d7519d6deac85
Globetrotter <r-u-there@outlook.com>
2024-10-18 11:02:15 +0000

    Update order of SQL statements for function creation

--- views.sql
+++ views.sql
@@ -13,35 +13,8 @@ alter table nodes cluster on "index_parentnode_id";
 create index if not exists index_object_tag_order on taggings(object_id, tag_id);
 create index if not exists index_tag_object_order on taggings(tag_id, object_id);
 
--- Full materialisation of parent_nodes --> nodes --> tags --> objects
-drop materialized view if exists nodes_taggings cascade;
-create materialized view nodes_taggings 
-as
-select H.parentnode_id, H.id as node_id, H.tag_id, R.object_id
-from (select N.parentnode_id, N.id, (get_subtree_from_parent_node(N.id)).tag_id from nodes N) H
-	join taggings R on R.tag_id = H.tag_id;
-
--- Full materialisation of tagsets --> tags --> objects
-drop materialized view if exists tagsets_taggings cascade;
-create materialized view tagsets_taggings
-as
-select T.tagset_id as tagset_id, R.tag_id, R.object_id
-from tags T 
-    join taggings R on R.tag_id = T.id;
-
--- Index on parent_nodes --> objects --> nodes (for grouping by child nodes)
-create index nodes_taggings_pid_oid_nid on nodes_taggings(parentnode_id, object_id, node_id);
--- Index on nodes --> objects (for retrieving cells)
-create index nodes_taggings_nid_oid on nodes_taggings(node_id, object_id);
--- Index on tagsets --> objects --> tags (tags matter here)
-create index tagsets_taggings_sid_oid_tid on tagsets_taggings(tagset_id, object_id, tag_id);
--- Index on tags --> objects
--- Currently, queries are run against taggings which has the same information
--- create index tagsets_taggings_tid_oid on tagsets_taggings(tag_id, object_id);
-
-
 -- Returns all nodes in the subtree from (parent node, hierarchy)
--- drop function if exists get_subtree_from_parent_node(int) cascade;
+drop function if exists get_subtree_from_parent_node(int) cascade;
 create function get_subtree_from_parent_node(int) 
 returns setof nodes 
 as '
@@ -76,5 +49,31 @@ as '
 		join nodes N1 on N.parentnode_id = N1.parentnode_id
 	where N1.id = $1 and N1.hierarchy_id = $2
 ' language SQL;
+
+-- Full materialisation of parent_nodes --> nodes --> tags --> objects
+drop materialized view if exists nodes_taggings cascade;
+create materialized view nodes_taggings 
+as
+select H.parentnode_id, H.id as node_id, H.tag_id, R.object_id
+from (select N.parentnode_id, N.id, (get_subtree_from_parent_node(N.id)).tag_id from nodes N) H
+	join taggings R on R.tag_id = H.tag_id;
+
+-- Full materialisation of tagsets --> tags --> objects
+drop materialized view if exists tagsets_taggings cascade;
+create materialized view tagsets_taggings
+as
+select T.tagset_id as tagset_id, R.tag_id, R.object_id
+from tags T 
+    join taggings R on R.tag_id = T.id;
+
+-- Index on parent_nodes --> objects --> nodes (for grouping by child nodes)
+create index nodes_taggings_pid_oid_nid on nodes_taggings(parentnode_id, object_id, node_id);
+-- Index on nodes --> objects (for retrieving cells)
+create index nodes_taggings_nid_oid on nodes_taggings(node_id, object_id);
+-- Index on tagsets --> objects --> tags (tags matter here)
+create index tagsets_taggings_sid_oid_tid on tagsets_taggings(tagset_id, object_id, tag_id);
+-- Index on tags --> objects
+-- Currently, queries are run against taggings which has the same information
+-- create index tagsets_taggings_tid_oid on tagsets_taggings(tag_id, object_id);
 
 SELECT NOW();
