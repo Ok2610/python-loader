965b0a0c6007ffad41a5ea6b0dde8634ed6f545c
Gwendal Trehin <gwendal.trehin@etu.univ-grenoble-alpes.fr>
2025-08-05 14:46:05 +0000

    add time to the logfiles entries

--- client/filemgmt/filehandler.py
+++ client/filemgmt/filehandler.py
@@ -6,4 +6,4 @@ class FileHandler:
     def __init__(self) -> None:
         self.client  = grpc_client.LoaderClient()
         logger = logging.getLogger(__name__)
-        logging.basicConfig(filename='execution.log', encoding='utf-8', level=logging.DEBUG)
+        logging.basicConfig(filename='execution.log', format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', encoding='utf-8', level=logging.DEBUG)
